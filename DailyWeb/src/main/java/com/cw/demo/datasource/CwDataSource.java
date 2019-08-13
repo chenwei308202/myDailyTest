b67a0098e3ce7183d5a550db7dcecdaa5d38907a
chenwei <chenwei01@Koolearn-inc.com>
2019-08-13 11:06:23 +0800

    令牌桶demo

---
+++ DailyWeb/src/main/java/com/cw/demo/datasource/CwDataSource.java
@@ -0,0 +1,11 @@
+package com.cw.demo.datasource;
+
+/**
+ * 自定义的数据源
+ *
+ * @author chenwei
+ * @create 2018-09-21 14:40
+ **/
+
+public class CwDataSource {
+}
---
+++ DailyWeb/src/main/java/com/cw/demo/datasource/intercepter/MySqlInterceptor.java
@@ -0,0 +1,9 @@
+package com.cw.demo.datasource.intercepter;
+
+/**
+ * @author chenwei
+ * @create 2018-09-21 14:55
+ **/
+
+public class MySqlInterceptor {
+}
